b59a847bbded360ddcd53040766459592c5c8881
benson_liao <benson_liao@mail.chungyo.net>
2025-05-06 17:38:39 +0800

    🍻 improve Button

--- packages/ui/src/components/icons/Icon.vue
+++ packages/ui/src/components/icons/Icon.vue
@@ -7,6 +7,7 @@ const IconEnum = {
   info: 2,
   warning: 3,
   error: 4,
+  google: 5,
 }
 export type IconTypes = (keyof typeof IconEnum);
 const props = <{ variant: IconTypes }>defineProps({
@@ -32,6 +33,9 @@ console.log('variant' , props.variant);
     </svg>
     <svg v-if="IconEnum[props.variant] === IconEnum.error" class="w-6 h-6 text-white fill-current" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
       <path d="M20 3.36667C10.8167 3.36667 3.3667 10.8167 3.3667 20C3.3667 29.1833 10.8167 36.6333 20 36.6333C29.1834 36.6333 36.6334 29.1833 36.6334 20C36.6334 10.8167 29.1834 3.36667 20 3.36667ZM19.1334 33.3333V22.9H13.3334L21.6667 6.66667V17.1H27.25L19.1334 33.3333Z" />
+    </svg>
+    <svg v-if="IconEnum[props.variant] === IconEnum.google" class="w-[48px] h-[48px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+      <path fill-rule="evenodd" d="M12.037 21.998a10.313 10.313 0 0 1-7.168-3.049 9.888 9.888 0 0 1-2.868-7.118 9.947 9.947 0 0 1 3.064-6.949A10.37 10.37 0 0 1 12.212 2h.176a9.935 9.935 0 0 1 6.614 2.564L16.457 6.88a6.187 6.187 0 0 0-4.131-1.566 6.9 6.9 0 0 0-4.794 1.913 6.618 6.618 0 0 0-2.045 4.657 6.608 6.608 0 0 0 1.882 4.723 6.891 6.891 0 0 0 4.725 2.07h.143c1.41.072 2.8-.354 3.917-1.2a5.77 5.77 0 0 0 2.172-3.41l.043-.117H12.22v-3.41h9.678c.075.617.109 1.238.1 1.859-.099 5.741-4.017 9.6-9.746 9.6l-.215-.002Z" clip-rule="evenodd"/>
     </svg>
     <slot v-if="slots.default"></slot>
   </div>
