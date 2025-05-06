e544fa992ee8d5ed602aa447cd8e0b0699d2db9a
benson_liao <benson_liao@mail.chungyo.net>
2025-05-06 17:34:37 +0800

    🍻 improve Button

---
+++ packages/ui/src/components/button/RadioButton.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="group cursor-default w-[238px] h-[188px] p-2 text-sm bg-gray-500 rounded-md">
+    <div>Marketing</div>
+    <div class="flex flex-col items-center justify-between">
+      <div class="w-full flex items-center gap-1 rounded-sm hover:bg-[rgba(255,255,255,0.5)] px-2">
+        <input class="w-5 h-5
+        bg-gray-100 border-gray-300
+        grow-0" type="checkbox" id="checkbox1"/>
+        <label class="w-full grow" for="checkbox1">Social Media Campaigns</label>
+<!--        <div class="w-full grow bg-amber-300">Social Media Campaigns</div>-->
+        <div class="grow-0 ">23</div>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
