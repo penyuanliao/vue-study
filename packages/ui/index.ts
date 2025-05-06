c04a55120c2ae4648f4782eeae214259193e47ef
benson_liao <benson_liao@mail.chungyo.net>
2025-05-06 17:40:55 +0800

    🍻 improve Button

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -1,5 +1,5 @@
 import Button from "./src/components/Button.vue";
-import Dropdown from "./src/components/dropdown/Dropdown.vue";
+import DropdownIcon from "./src/components/dropdown/DropdownIcon.vue";
 import DarkMode from "./src/components/DarkMode.vue";
 import Login from "./src/components/login/Login.vue";
 import Simple from "./src/components/breadcrumbs/Simple.vue";
@@ -10,8 +10,10 @@ import EditTable from './src/components/tables/EditTable.vue';
 import icon, { type IconTypes } from "./src/components/icons/Icon.vue";
 import ButtonGroupIcon from './src/components/button/ButtonGroupIcon.vue'
 import SidebarCollapse from './src/components/sidebar/SidebarCollapse.vue'
+import TextFadeIn from './src/components/text/TextFadeIn.vue'
+import TextGradientStroke from './src/components/text/TextGradientStroke.vue'
 const Breadcrumbs = { Simple };
-export { Button, Dropdown, DarkMode, Login, Breadcrumbs, LoadingBase, ErrorComponent, EditTable, ImgIcon, icon, type IconTypes,
-  ButtonGroupIcon, SidebarCollapse,
+export { Button, DropdownIcon, DarkMode, Login, Breadcrumbs, LoadingBase, ErrorComponent, EditTable, ImgIcon, icon, type IconTypes,
+  ButtonGroupIcon, SidebarCollapse, TextFadeIn, TextGradientStroke
 };
 
